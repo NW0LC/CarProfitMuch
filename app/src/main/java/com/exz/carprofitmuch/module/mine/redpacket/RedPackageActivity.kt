b2917c60ff5fbf64470e37606f1f0c1654a8aeca
xzw <1565479418@qq.com>
2017-11-09 18:05:01 +0800

    优惠券-红包-评价

---
+++ app/src/main/java/com/exz/carprofitmuch/module/mine/redpacket/RedPackageActivity.kt
@@ -0,0 +1,52 @@
+package com.exz.carprofitmuch.module.mine.redpacket
+
+import android.support.v4.app.Fragment
+import com.exz.carprofitmuch.R
+import com.exz.carprofitmuch.bean.TabEntity
+import com.flyco.tablayout.listener.CustomTabEntity
+import com.szw.framelibrary.base.BaseActivity
+import com.szw.framelibrary.utils.StatusBarUtil
+import kotlinx.android.synthetic.main.action_bar_custom.*
+import kotlinx.android.synthetic.main.activity_comment_list.*
+import java.util.*
+
+/**
+ * Created by pc on 2017/11/9.
+ * 红包
+ */
+
+class RedPackageActivity : BaseActivity() {
+    private val mTitles = arrayOf("未使用", "已使用", "已过期")
+    private val mIconUnSelectIds = intArrayOf(R.mipmap.icon_home_off, R.mipmap.icon_store_off, R.mipmap.icon_goods_car_off, R.mipmap.icon_mine_off)
+    private val mIconSelectIds = intArrayOf(R.mipmap.icon_home_on, R.mipmap.icon_store_on, R.mipmap.icon_goods_car_on, R.mipmap.icon_mine_on)
+    private val mTabEntities = ArrayList<CustomTabEntity>()
+    private val mFragments = ArrayList<Fragment>()
+    override fun initToolbar(): Boolean {
+        mTitle.text = getString(R.string.mine_coupon_class_name)
+        //状态栏透明和间距处理
+        StatusBarUtil.immersive(this)
+        StatusBarUtil.setPaddingSmart(this, toolbar)
+        StatusBarUtil.setPaddingSmart(this, blurView)
+        return false
+    }
+
+    override fun setInflateId(): Int = R.layout.activity_comment_list
+
+    override fun init() {
+        initTabBar()
+        initEvent()
+    }
+
+    private fun initTabBar() {
+        mTitles.indices.mapTo(mTabEntities) { TabEntity(mTitles[it], mIconSelectIds[it], mIconUnSelectIds[it]) }
+        mFragments.add(RedPackageListFragment.newInstance(0))
+        mFragments.add(RedPackageListFragment.newInstance(1))
+        mFragments.add(RedPackageListFragment.newInstance(2))
+        mTabLayout.setTabData(mTabEntities, this, R.id.frameLayout, mFragments)
+    }
+
+
+    private fun initEvent() {
+        toolbar.setNavigationOnClickListener { finish() }
+    }
+}
